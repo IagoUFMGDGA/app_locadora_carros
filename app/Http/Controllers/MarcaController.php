8d733b4e8341394ab736f0dba227e47734dcead2
IagoUFMGDGA <iagoUFMG@gmail.com>
2022-08-25 16:56:00 -0300

    Preparando as actions nos controllers para testar os endpoints no postman

--- app/Http/Controllers/MarcaController.php
+++ app/Http/Controllers/MarcaController.php
@@ -14,17 +14,7 @@ class MarcaController extends Controller
      */
     public function index()
     {
-        //
-    }
-
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
+        return  Marca::all();
     }
 
     /**
@@ -35,7 +25,8 @@ class MarcaController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $marca = Marca::create($request->all());
+        return $marca;
     }
 
     /**
@@ -46,18 +37,7 @@ class MarcaController extends Controller
      */
     public function show(Marca $marca)
     {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Models\Marca  $marca
-     * @return \Illuminate\Http\Response
-     */
-    public function edit(Marca $marca)
-    {
-        //
+        return $marca;
     }
 
     /**
@@ -69,7 +49,8 @@ class MarcaController extends Controller
      */
     public function update(Request $request, Marca $marca)
     {
-        //
+        $marca->update($request->all());
+        return $marca; 
     }
 
     /**
@@ -80,6 +61,7 @@ class MarcaController extends Controller
      */
     public function destroy(Marca $marca)
     {
-        //
+        $marca->delete();
+        return ['msg' => 'Deleção concluída com sucesso!'];
     }
 }
