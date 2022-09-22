bb6e89a0b7f5c8e73676afe1253e80a874786655
IagoUFMGDGA <iagoUFMG@gmail.com>
2022-09-22 16:19:48 -0300

    implementando método login. aula: 332

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -6,8 +6,20 @@ use Illuminate\Http\Request;
 
 class AuthController extends Controller
 {
-    public function login(){
-        return 'loging';
+    public function login(Request $request){
+        $credenciais = $request->all(['email', 'password']);
+        
+        // autenticação (email senha)
+        $token = auth('api')->attempt($credenciais); // reuper auth
+        
+        // retornar um JWT (Json Web Token)
+        if($token){
+            return response()->json(['token'=>$token], 200);
+        }else{
+            return response()->json(['erro' => 'usuário ou senha inválido'], 403);
+            // 401 = Unauthorized -> não autorizado
+            // 403 = forbidden -> proibido (login errado)
+        }
     }
     public function logout(){
         return 'logout';
